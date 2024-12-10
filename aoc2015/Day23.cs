22f8333395c8de76f1f92c32cf46ba5b4a892788
Bengt Sirbelius <bengto@axis.com>
2024-12-10 15:31:43 +0100

    Day 23 done (not 22)

--- aoc2015/Day23.cs
+++ aoc2015/Day23.cs
@@ -9,22 +9,127 @@ namespace aoc2015
 {
     internal class Day23
     {
+        internal int Solve(List<string>[] values, bool isPart2)
+        {
+            int a = isPart2 ? 1 : 0;
+            int b = 0;
+
+            int pc = 0;
+            int offset = 0;
+
+            while (pc >= 0 && pc < values.Length)
+            {
+                switch (values[pc][0])
+                {
+                    case "hlf":
+                        if (values[pc][1] == "a")
+                        {
+                            a /= 2;
+                        }
+                        else
+                        {
+                            b /= 2;
+                        }
+                        pc++;
+                        break;
+                    case "tpl":
+                        if (values[pc][1] == "a")
+                        {
+                            a *= 3;
+                        }
+                        else
+                        {
+                            b *= 3;
+                        }
+                        pc++;
+                        break;
+                    case "inc":
+                        if (values[pc][1] == "a")
+                        {
+                            a++;
+                        }
+                        else
+                        {
+                            b++;
+                        }
+                        pc++;
+                        break;
+                    case "jmp":
+                        offset = Int32.Parse(values[pc][1]);
+                        pc += offset;
+                        break;
+                    case "jie":
+                        offset = Int32.Parse(values[pc][2]);
+                        if (values[pc][1] == "a")
+                        {
+                            if ((a % 2) == 0)
+                            {
+                                pc += offset;
+                            }
+                            else
+                            {
+                                pc += 1;
+                            }
+                        }
+                        else
+                        {
+                            if ((b % 2) == 0)
+                            {
+                                pc += offset;
+                            }
+                            else
+                            {
+                                pc += 1;
+                            }
+                        }
+                        break;
+                    case "jio":
+                        offset = Int32.Parse(values[pc][2]);
+                        if (values[pc][1] == "a")
+                        {
+                            if (a == 1)
+                            {
+                                pc += offset;
+                            }
+                            else
+                            {
+                                pc += 1;
+                            }
+                        }
+                        else
+                        {
+                            if (b == 1)
+                            {
+                                pc += offset;
+                            }
+                            else
+                            {
+                                pc += 1;
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return b;
+        }
+
         public void Part1()
         {
             var data = File.ReadAllLines(@"data\day23.txt");
 
-            var values = data.Select(s => s.Select(c => c).ToArray()).ToArray();
+            var values = data.Select(s => s.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList()).ToArray();
 
-            Console.WriteLine($"Answer is {42}");
+            Console.WriteLine($"Answer is {Solve(values, false)}");
         }
 
         public void Part2()
         {
             var data = File.ReadAllLines(@"data\day23.txt");
 
-            var values = data.Select(s => s.Select(c => c).ToArray()).ToArray();
+            var values = data.Select(s => s.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList()).ToArray();
 
-            Console.WriteLine($"Answer is {42}");
+            Console.WriteLine($"Answer is {Solve(values, true)}");
         }
     }
 }
--- aoc2015/Program.cs
+++ aoc2015/Program.cs
@@ -10,9 +10,9 @@ namespace aoc2015
     {
         static void Main(string[] args)
         {
-            new Day20().Part1();
+            new Day23().Part1();
 
-            new Day20().Part2();
+            new Day23().Part2();
 
             Console.ReadLine();
         }
